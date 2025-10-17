933ba1d25d3afd95696537ae8c8c14f57226aec9
Ayush20076564 <20076564@mydbs.ie>
2025-10-17 11:31:14 +0100

    Practise Questions

---
+++ Pattern6.java
@@ -0,0 +1,14 @@
+public class Pattern6 {
+    public static void main(String args[]){
+        int n =5;
+        int j = 0;
+        for(int i = 1; i <= n; i++){
+            for(j=1 ; j<=i; j++){
+                System.out.print(j+" ");
+                
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ Pattern7.java
@@ -0,0 +1,13 @@
+public class Pattern7 {
+    public static void main(String args[]){
+        int n = 5;
+        int j = 0;
+        for(int i=1; i<=n; i++){
+            for(j=1;j<=n-i+1;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
